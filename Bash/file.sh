41ee652a2a0b9d12311e3ef6fc5e56d08b0645ee
Bariq <Adebariq@gmail.com>
2024-11-04 23:00:59 +0000

    My first bash script project

---
+++ Bash/file.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+echo "my first bash script"
+
+name="bariq"
+age=10
+location="arsenal"
+
+echo "$hi $name. you are $age. you live $location"
+
+ 
+
---
+++ Bash/test_project.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+mkdir /home/bariq/Desktop/Bash/my_proj
+cd /home/bariq/Desktop/Bash/my_proj
+touch /home/bariq/Desktop/Bash/my_proj/file1.txt file2.txt file3.txt
+ls /home/bariq/Desktop/Bash/my_proj
+echo "Project set_up complete"
+ 
+
+
+
